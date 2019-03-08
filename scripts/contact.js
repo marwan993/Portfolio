915d39f4335de4de5ccfaa023e55ff609835d264
Marwan Alaloosi <manoalosi@gmail.com>
2019-03-07 18:08:28 -0600

    Done

---
+++ scripts/contact.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.submit').click(function(event){
+// event.preventDefault()
+console.log('Clicked button')
+
+let name = $('.name').val()
+let email = $('.email').val()
+let message = $('.message').val()
+
+
+    })
+})
